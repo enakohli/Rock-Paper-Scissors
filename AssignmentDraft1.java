7b84b1cc06b9dd916c19266c3af767347ae69b73
Ena Kohli <72359254+enakohli@users.noreply.github.com>
2021-05-26 21:00:51 -0400

    Initial commit

---
+++ AssignmentDraft1.java
@@ -0,0 +1,38 @@
+/* 
+ * Name: Ena Kohli
+ * Program Name: Unit Assignment 
+ * Date: April 30th 2018
+ * Purpose: To ask the user to select an option from the main menu and act accordingly
+ */
+
+import java.util.*;
+public class AssignmentDraft1 {
+
+	public static void main(String[] args) {
+		//Declare Variables
+		String object = "", quit1 = "";
+		int computerObject = 0, option = 0;
+		
+		//Import the scanner 
+		Scanner input = new Scanner (System.in);
+		
+		//Output the options for the user to select
+		System.out.println("****Welcome to the Main Menu!!!****");
+		System.out.println("1. Rock Paper Scissors");
+		System.out.println("2. String Handling Functions");
+		System.out.println("3. Guess the Number");
+		System.out.println("4. Exit");
+			
+		//Ask the user to enter an option
+		System.out.println("Enter an option from the options listed (select the number):");
+		option = input.nextInt();
+		
+		
+			
+		
+		
+		
+
+	}
+
+}
